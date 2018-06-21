b80bb9e792d28d3afb48512a7ce6723613c40803
baobaojianqiang <16230091@qq.com>
2018-06-21 15:24:29 +0800

    1v

---
+++ HostApp/src/main/java/jianqiang/com/hostapp/MyApplication.java
@@ -0,0 +1,29 @@
+package jianqiang.com.hostapp;
+
+import android.app.Application;
+import android.content.Context;
+
+import com.example.jianqiang.mypluginlibrary.PluginManager;
+import com.example.jianqiang.mypluginlibrary.Utils;
+
+import java.io.File;
+
+import dalvik.system.DexClassLoader;
+import jianqiang.com.hostapp.ams_hook.AMSHookHelper;
+
+public class MyApplication extends Application {
+
+    @Override
+    protected void attachBaseContext(Context newBase) {
+        super.attachBaseContext(newBase);
+
+        PluginManager.init(this);
+
+        try {
+            AMSHookHelper.hookAMN();
+            AMSHookHelper.hookActivityThread();
+        } catch (Throwable e) {
+            e.printStackTrace();
+        }
+    }
+}
